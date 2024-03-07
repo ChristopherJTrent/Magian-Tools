de90bcc22ce3d12b65586147a212628519eb3814
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-07 15:58:31 -0800

    build and style feedback sidebar

--- src/js/components/meter.js
+++ src/js/components/meter.js
@@ -1,5 +1,6 @@
 import { Component } from "../lib/Nyzul/index.mjs";
 import { sum } from "../lib/Storehouse/data/aggregators.js";
+import { Storehouse } from "../lib/Storehouse/index.mjs";
 
 
 export default class Meter extends Component {
@@ -10,8 +11,36 @@ export default class Meter extends Component {
      * @param {boolean|Function} [aggregate=true] set to false for no aggregation, true for default (sum), and pass a callback for custom.
      */
     constructor(label, key, aggregate = true) {
+        super()
         if (aggregate === true || typeof aggregate === 'function') { 
-            if (aggregate === true ) aggregate = sum;
+            if (aggregate === true ) aggregate = sum
+            this.useAggregation = true;
+        } else {
+            this.useAggregation = false;
+        }
+        this.aggregate = aggregate;
+        this.label = label;
+        this.key = key;
+    }
+    render() {
+        const label = document.createElement('label')
+        label.setAttribute('for', `meter-${this.key}`)
+        label.innerText = this.label
+        this.appendChild(label)
+        const meter = document.createElement('meter');
+        meter.id = `meter-${this.key}`
+        this.registerConsumer(meter)
+        this.appendChild(meter)
+        return this.element;
+    }
+    registerConsumer(meter) {
+        const _meterCallback = (value) => {
+            meter.setAttribute('value', value.toString())
+        }
+        if (this.useAggregation) {
+            Storehouse.registerAggregateSubscriber(this.key, _meterCallback, this.aggregate)
+        } else {
+            Storehouse.registerSubscriber(this.key, _meterCallback)
         }
     }
 }
--- src/js/layouts/feedback-sidebar.js
+++ src/js/layouts/feedback-sidebar.js
@@ -1,3 +1,4 @@
+import Meter from "../components/meter.js";
 import Component from "../lib/Nyzul/engine/component.js";
 
 export default class FeedbackSidebar extends Component {
@@ -7,7 +8,7 @@ export default class FeedbackSidebar extends Component {
         this.element.classList.add("materialCard");
     }
     render() {
-
+        this.appendComponent(new Meter('Equipment Haste', 'haste'))
         return this.element
     }
 }
