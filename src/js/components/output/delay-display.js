1a4604591edc3029110f5937c7d128f1151bd063
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-11 20:52:37 -0700

    delayDisplay minimal class scaffold

---
+++ src/js/components/output/delay-display.js
@@ -0,0 +1,11 @@
+import { Component } from "../../lib/Nyzul/index.mjs";
+
+
+export default class DelayDisplay extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        
+    }
+}
