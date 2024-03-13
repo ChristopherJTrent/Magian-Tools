1196ade3f127fcb82247df5fc91080f10fd75524
ChristopherJTrent <ChristopherJTrent@outlook.com>
2024-03-13 13:57:29 -0700

    refactor disgusting html generation code

--- src/js/components/input/white-blue-magic-haste.js
+++ src/js/components/input/white-blue-magic-haste.js
@@ -47,50 +47,34 @@ export default class WhiteBlueMagicHaste extends Component {
     ]
     constructor() {
         super();
+        this.element = document.createElement('form')
+
         this.element.id = 'white-blue-magic-haste';
 
         this.eventCallback = this.handleInputEvent.bind(this);
-        this.embravaCallback = this.embrava.bind(this)
-        this.mightyGuardCallback= this.mightyGuard.bind(this)
+        this.embravaCallback = this.embravaHandler.bind(this)
+        this.mightyGuardCallback= this.mightyGuardHandler.bind(this)
 
         this.element.classList.add('flexContainer', 'vertical');
         [this.setProvider, this.getProvider] = Storehouse.registerArrayProvider('wb-magic-haste', [0,0,0])
     }
     render() {
-        const containingBlock = document.createElement('div')
+        const containingBlock = document.createElement('fieldset')
+        containingBlock.classList.add('flexContainer', 'vertical')
+        const legend = document.createElement('legend')
+        legend.innerText = 'White/Blue Magical Haste'
+        containingBlock.appendChild(legend)
         for (const def of WhiteBlueMagicHaste.FIELD_DEFS) {
-            this.element.innerHTML += `
-            <label for="${def.id}">
-                <input type="radio" name="${this.element.id}" value="${def.value}" ${def.default ? "checked" : ""} id="${def.id}">
-                ${def.label}
-            </label>`
-            setTimeout(() => document.getElementById(def.id).addEventListener('input', this.eventCallback), 10)
+            containingBlock.appendChild(this.hasteSpell(def))
         }
         this.setProvider(0,0)
-        this.appendChild(document.createElement('hr'))
-        const mightyGuard = document.createElement('label')
-        mightyGuard.setAttribute('for', 'mightyGuard')
-        
-        const mgCheckbox = document.createElement('input')
-        mgCheckbox.setAttribute('name', 'mighty-guard')
-        mgCheckbox.setAttribute('type', 'checkbox')
-        mgCheckbox.id = 'mightyGuard'
-        mightyGuard.appendChild(mgCheckbox)
-        mightyGuard.innerHTML += '\nMighty Guard'
-        setTimeout(() => document.getElementById('mightyGuard').addEventListener('input', this.mightyGuardCallback))
-        this.appendChild(mightyGuard)
+        containingBlock.appendChild(this.mightyGuard())
         this.setProvider(1, 0)
-        const embrava = document.createElement('label')
-        embrava.setAttribute('for', 'embrava')
-        const embravaCheckbox = document.createElement('input')
-        embravaCheckbox.setAttribute('name', 'embrava')
-        embravaCheckbox.setAttribute('type', 'checkbox')
-        embravaCheckbox.id = 'embrava'
-        embrava.appendChild(embravaCheckbox)
-        embrava.innerHTML += '\n Embrava'
-        setTimeout(() => document.getElementById('embrava').addEventListener('input', this.embravaCallback))
-        this.appendChild(embrava)
+
+        containingBlock.appendChild(this.embrava())
         this.setProvider(2, 0)
+
+        this.appendChild(containingBlock)
         return this.finalize();
     }
     /**
@@ -101,7 +85,50 @@ export default class WhiteBlueMagicHaste extends Component {
         e.stopPropagation()
         this.setProvider(0, parseInt(e.target.value))
     }
-    embrava(e) {
+
+    hasteSpell(def) {
+        const label = document.createElement('label')
+        const input = document.createElement('input')
+        label.setAttribute('for', def.id)
+        input.id = def.id
+        input.name = this.element.id
+        input.value = def.value;
+        input.type = 'radio'
+        if (def.default) input.defaultChecked = true
+        input.addEventListener('change', this.eventCallback)
+        label.appendChild(input)
+        label.append(def.label)
+        return label
+    }
+
+    embrava() {
+        const embrava = document.createElement('label')
+        embrava.setAttribute('for', 'embrava')
+        const embravaCheckbox = document.createElement('input')
+        embravaCheckbox.setAttribute('name', 'embrava')
+        embravaCheckbox.setAttribute('type', 'checkbox')
+        embravaCheckbox.id = 'embrava'
+        embrava.appendChild(embravaCheckbox)
+        embrava.append('\n Embrava')
+        embrava.addEventListener('change', this.embravaCallback)
+        return embrava
+    }
+
+    mightyGuard() {
+        const mightyGuard = document.createElement('label')
+        mightyGuard.setAttribute('for', 'mightyGuard')
+        
+        const mgCheckbox = document.createElement('input')
+        mgCheckbox.setAttribute('name', 'mighty-guard')
+        mgCheckbox.setAttribute('type', 'checkbox')
+        mgCheckbox.id = 'mightyGuard'
+        mightyGuard.appendChild(mgCheckbox)
+        mightyGuard.append('\nMighty Guard')
+        mgCheckbox.addEventListener('change', this.mightyGuardCallback)
+        return mightyGuard
+    }
+
+    embravaHandler(e) {
         e.stopPropagation()
         if(e.target.checked) {
             this.setProvider(2, 266)
@@ -109,7 +136,7 @@ export default class WhiteBlueMagicHaste extends Component {
             this.setProvider(2, 0)
         }
     }
-    mightyGuard(e) {
+    mightyGuardHandler(e) {
         e.stopPropagation()
         if(e.target.checked) {
             this.setProvider(1, 150)
